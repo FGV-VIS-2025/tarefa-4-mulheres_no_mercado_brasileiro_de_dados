8b5c00a3373b08abc377559863aa97897f94023c
paula lima <paulaeduardalima09@gmail.com>
2025-04-24 10:36:18 -0300

    Ajusta layout e adiciona título e introdução

--- sections.js
+++ sections.js
@@ -15,7 +15,7 @@ let dataset = [];
 
 // Tamanho e margens
 const margin = { top: 50, right: 50, bottom: 50, left: 50 };
-const width = 800 - margin.left - margin.right;
+const width = 950 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
 
 // Escalas (serão preenchidas depois)
